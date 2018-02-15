864110391d83325f8ea5c632818a1ad8766b31e8
loich79 <loic.hay79@gmail.com>
2018-02-15 21:49:38 +0100

    Ajout d'un formulaire pour la saisie du code barre

--- code_barre.php
+++ code_barre.php
@@ -1,32 +1,43 @@
 <?php
-    // initialisation des variables
-    $barCode = "303792016200";
-    $sum = 0;
-    // parcourt la suite de chiffre du code barre
-    for ($i = 0; $i<strlen($barCode); $i++) {
-        // si l'itération est paire
-        if($i%2) {
-            //  somme = somme + le chiffre de l'itération * 3
-            $sum = $sum + ($barCode[$i] * 3);;
-        //sinon
-        } else {
-            //  somme = somme + le chiffre de l'itération * 3
-            $sum = $sum + $barCode[$i];
-        }
-        // fin de condition
-    }
-    //fin de parcours
-    //Modulo de la somme = somme modulo 10
-    $sumModulus = $sum%10;
-
-    // si le modulo de la somme = 0
-    if($sumModulus === 0) {
-        // la clé du code barre  = 0
-        $barCodeKey = 0;
-    //sinon
+    // teste les données récupérées par le formulaire
+    if (isset($_POST['barCode'])) {
+       $barCode = $_POST['barCode'];
     } else {
-        //la clé du code barre = 10 - modulo de la somme
-        $barCodeKey = 10 - $sumModulus;
+        $barCode = "";
+    }
+
+    // initialisation de la sommme et de la clé
+    $sum = 0;
+    $barCodeKey = "";
+
+    // teste si le code barre existe
+    if ($barCode !== "") {
+                // parcourt la suite de chiffre du code barre
+        for ($i = 0; $i < strlen($barCode); $i++) {
+            // si l'itération est paire
+            if ($i % 2) {
+                //  somme = somme + le chiffre de l'itération * 3
+                $sum = $sum + ($barCode[$i] * 3);;
+                //sinon
+            } else {
+                //  somme = somme + le chiffre de l'itération * 3
+                $sum = $sum + $barCode[$i];
+            }
+            // fin de condition
+        }
+        //fin de parcours
+        //Modulo de la somme = somme modulo 10
+        $sumModulus = $sum % 10;
+
+        // si le modulo de la somme = 0
+        if ($sumModulus === 0) {
+            // la clé du code barre  = 0
+            $barCodeKey = 0;
+            //sinon
+        } else {
+            //la clé du code barre = 10 - modulo de la somme
+            $barCodeKey = 10 - $sumModulus;
+        }
     }
 ?>
 
@@ -39,8 +50,15 @@
 
     <body>
         <h1>Calcul de la clé d'un code barre</h1>
-        <p>votre code barre : <?= $barCode ?></p>
+        <form method="POST">
+            <label for="barCode">Saississez les 12 premiers chiffres de votre code barre : </label>
+            <input type="text" id="barCode" name="barCode"/>
+            <input type="submit" value="Ok">
+        </form>
+        <p>votre saisie : <?= $barCode ?></p>
         <p>votre clé de code barre : <?= $barCodeKey ?></p>
+
+        <p>votre code barre : <?= $barCode." ". $barCodeKey ?></p>
 
     </body>
 </html>
